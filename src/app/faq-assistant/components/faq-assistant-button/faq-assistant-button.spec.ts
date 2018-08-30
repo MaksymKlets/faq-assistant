ff6b3a0ac9be64114e7ab4e41addd52db9a3c9bb
mklets <maksym.klets@sigma.software>
2018-08-30 13:08:15 +0300

    Changed name of service and method for unit test.

--- src/app/faq-assistant/components/faq-assistant-button/faq-assistant-button.spec.ts
+++ src/app/faq-assistant/components/faq-assistant-button/faq-assistant-button.spec.ts
@@ -1,20 +1,20 @@
 import {FaqAssistantButtonComponent} from './faq-assistant-button.component';
-import {CommunicationFaqAssistantService} from '../../services/faq-assistant.service';
+import {FaqAssistantService} from '../../services/faq-assistant.service';
 
 describe('FaqAssistantButtonComponent', function () {
   it('should create instance of FaqAssistantButtonComponent', () => {
-    expect(new FaqAssistantButtonComponent(new CommunicationFaqAssistantService())).toBeTruthy();
+    expect(new FaqAssistantButtonComponent(new FaqAssistantService())).toBeTruthy();
   });
 
   it('isContainerVisible should has "false" value', () => {
-    const faqButton = new FaqAssistantButtonComponent(new CommunicationFaqAssistantService());
+    const faqButton = new FaqAssistantButtonComponent(new FaqAssistantService());
 
     expect(faqButton.isContainerVisible).toEqual(false);
   });
 
   it('showHideFaqAssistantContainer method should change isContainerVisible value to true', () => {
-    const faqButton = new FaqAssistantButtonComponent(new CommunicationFaqAssistantService());
-    faqButton.showHideFaqAssistantContainer();
+    const faqButton = new FaqAssistantButtonComponent(new FaqAssistantService());
+    faqButton.toggleFaqAssistantContainer();
 
     expect(faqButton.isContainerVisible).toEqual(true);
   });
